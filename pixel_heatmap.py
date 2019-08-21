42487f8e4346127cb19c607258ab569c7861aee0
Serlopal <oro_sergio@hotmail.com>
2019-08-21 22:37:54 +0100

    added reset button and sliders to change live the kernel size and heat speed. Also changed normalization of kernel to increase variance inside it.

--- pixel_heatmap.py
+++ pixel_heatmap.py
@@ -1,12 +1,43 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QFileDialog, QTextEdit,\
-	QGridLayout, QGroupBox, QPushButton, QMainWindow, QDesktopWidget, QWidget, QVBoxLayout, QComboBox, \
-	QHBoxLayout, QStyleOptionComboBox, QStyle, QStyleOptionViewItem, QStylePainter, QTabWidget
+	QGridLayout, QGroupBox, QPushButton, QMainWindow, QDesktopWidget, QWidget, QVBoxLayout, QComboBox, QSlider
 from PyQt5.QtCore import Qt, QObject, pyqtSignal, pyqtSlot, QThread, QSize, QPoint
 from PyQt5.QtGui import QFont, QTextCursor, QTextDocument, QPalette, QAbstractTextDocumentLayout, QPainter, \
-			QBrush, QPen, QPalette, QColor, QImage
+			QBrush, QPen, QPalette, QColor, QImage, QPixmap
 import numpy as np
 from scipy.stats import norm
-from scipy.stats import norm
+
+
+class InfoSlider(QGroupBox):
+	def __init__(self, start, max, min):
+		super().__init__()
+
+		self._slider = QSlider()
+
+		self._slider.setMaximum(max)
+		self._slider.setMinimum(min)
+		self._slider.setValue(start)
+
+		self.curr = start
+		max_label = QLabel(str(max))
+		min_label = QLabel(str(min))
+		self.curr_label = QLabel(str(self.curr))
+
+		layout = QGridLayout()
+		layout.addWidget(max_label, 0, 0, 1, 1)
+		layout.addWidget(min_label, 9, 0, 1, 1)
+		layout.addWidget(self.curr_label, 5, 0, 1, 1)
+		layout.addWidget(self._slider, 0, 2, 10, 2)
+
+		self.setLayout(layout)
+
+	def on_slide(self, funcs):
+		for f in funcs:
+			self._slider.sliderMoved.connect(f)
+
+	def change_value(self, x):
+		self.curr = x
+		self.curr_label.setText(str(x))
+		self._slider.setValue(x)
 
 
 class PixelHeatMap(QWidget):
@@ -15,22 +46,21 @@ class PixelHeatMap(QWidget):
 
 		self.color_path = self.generate_color_path()
 
-		self.width = 400
-		self.height = 400
+		c = self.rect()
 
-		c = self.rect().center()
-		self.center = c.x() - self.width/2, c.y() - self.height/2
+		self.width = c.width()
+		self.height = c.height()
 
-		self.kernel_size = 31
-		self.krad = int((self.kernel_size - 1) / 2)
-		self.kernel = self.generate_gaussian_kernel(self.kernel_size)
+		self.krad = 15
+		self.kernel = None
+		self.generate_gaussian_kernel()
 
-		self.step = 16
+		self.speed = 16
 
-		self._heatmap = np.stack([np.zeros((self.width, self.height)),
-								  np.zeros((self.width, self.height)),
-								  255*np.ones((self.width, self.height))], axis=2).astype('uint8')
-		self._heatmap_index = np.zeros((self.width, self.height), dtype='uint32')
+		self._heatmap = np.stack([np.zeros((self.height, self.width)),
+								  np.zeros((self.height, self.width)),
+								  255*np.ones((self.height, self.width))], axis=2).astype('uint8')
+		self._heatmap_index = np.zeros((self.height, self.width), dtype='uint32')
 
 		def index2color(idx, path):
 			if idx < len(path):
@@ -41,6 +71,12 @@ class PixelHeatMap(QWidget):
 		self.index2color_vectorizer = np.vectorize(index2color, excluded=['path'], otypes=[np.ndarray], signature='()->(n)')
 
 		self.setMouseTracking(True)
+
+	def reset(self):
+		self._heatmap = np.stack([np.zeros((self.height, self.width)),
+								  np.zeros((self.height, self.width)),
+								  255*np.ones((self.height, self.width))], axis=2).astype('uint8')
+		self._heatmap_index = np.zeros((self.height, self.width), dtype='uint32')
 
 	@staticmethod
 	def generate_color_path():
@@ -63,11 +99,14 @@ class PixelHeatMap(QWidget):
 
 		return path
 
-	@staticmethod
-	def generate_gaussian_kernel(s):
+	def generate_gaussian_kernel(self):
 		def dist(p1, p2):
 			return np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
+		def min_max_scale(x, mx, mn):
+			return (x - mn)/(mx - mn)
+
+		s = self.krad*2 + 1
 		# check valid kernel size
 		assert (s-1) % 2 == 0
 
@@ -77,11 +116,11 @@ class PixelHeatMap(QWidget):
 			for j in range(s):
 				d = dist((r, r), (i, j))
 				if d < r:
-					norm_d = d/s if d != 0 else d
+					norm_d = d/r if d != 0 else d
 					kernel[i, j] = norm.pdf(norm_d)
 				else:
 					kernel[i, j] = 0
-		return kernel
+		self.kernel = kernel
 
 	def paintEvent(self, e):
 		qp = QPainter()
@@ -91,7 +130,7 @@ class PixelHeatMap(QWidget):
 
 	def draw_widget(self, qp):
 		heatmap = QImage(self._heatmap.tobytes(), self.width, self.height, 3*self.width, QImage.Format_RGB888)
-		qp.drawImage(*self.center, heatmap)
+		qp.drawImage(0, 0, heatmap)
 
 	def mouseMoveEvent(self, event):
 		coords = event.pos()
@@ -120,7 +159,7 @@ class PixelHeatMap(QWidget):
 				kernel = kernel[:-(v_after - self.height), :]
 				v_after = self.height
 
-			self._heatmap_index[v_before:v_after, h_before:h_after] += (kernel * self.step).astype('uint32')
+			self._heatmap_index[v_before:v_after, h_before:h_after] += (kernel * self.speed).astype('uint32')
 
 			mask = self._heatmap_index[v_before:v_after, h_before:h_after]
 
@@ -140,25 +179,20 @@ class HeatMapUI:
 		self.window = QMainWindow()
 		self.window.setWindowTitle(ui_name)
 		self.window.setObjectName(ui_name)
-		# set window geometry
-		ag = QDesktopWidget().availableGeometry()
-		self.window.move(int(ag.width()*0.15), int(ag.height()*0.05))
-		window_dim = 0.8
-		self.window.setMinimumWidth(int(ag.width()*window_dim))
-		self.window.setMinimumHeight(int(ag.height()*window_dim))
-		self.window.setMaximumWidth(int(ag.width()*window_dim))
-		self.window.setMaximumHeight(int(ag.height()*window_dim))
 
 		# create placeholders for widget
 		self.heatmap = None
 
-		self.heatmap_height = 80
-		self.heatmap_width = 80
-
-		self.nradius = 8
-		self.step_mod = 128
-
 		self.add_widgets()
+
+		# set window geometry
+		ag = QDesktopWidget().availableGeometry()
+		self.window.move(int(ag.width()*0.15), int(ag.height()*0.05))
+		factor = 1.2
+		self.window.setMinimumWidth(int(self.heatmap.width*factor))
+		self.window.setMinimumHeight(int(self.heatmap.height))
+		self.window.setMaximumWidth(int(self.heatmap.width*factor))
+		self.window.setMaximumHeight(int(self.heatmap.height))
 
 		# set layout inside window
 		self.window.show()
@@ -167,9 +201,36 @@ class HeatMapUI:
 		return self.app.exec()
 
 	def add_widgets(self):
-		self.heatmap = PixelHeatMap()
 		# set main group as central widget
-		self.window.setCentralWidget(self.heatmap)
+		main_group = QGroupBox()
+		self.window.setCentralWidget(main_group)
+
+		# create main groups's layout
+		main_layout = QGridLayout()
+
+		# add heatmap inside a label
+		self.heatmap = PixelHeatMap()
+		main_layout.addWidget(self.heatmap, 0, 0, 10, 10)
+
+		# add kernel size slider
+		krad_slider = InfoSlider(20, 100, 0)
+		krad_slider.on_slide([lambda x: setattr(self.heatmap, "krad", x if x % 2 else x-1),
+							   self.heatmap.generate_gaussian_kernel,
+							   lambda x: krad_slider.change_value(x)])
+		main_layout.addWidget(krad_slider, 0, 13, 9, 1)
+
+		speed_slider = InfoSlider(self.heatmap.speed, 100, 0)
+		speed_slider.on_slide([lambda x: setattr(self.heatmap, "speed", 2*x),
+							   lambda x: speed_slider.change_value(x)])
+		main_layout.addWidget(speed_slider, 0, 12, 9, 1)
+
+		# add reset heatmap button
+		reset_button = QPushButton("Reset")
+		reset_button.clicked.connect(self.heatmap.reset)
+		main_layout.addWidget(reset_button, 9, 12, 1, 1)
+
+		# set main group's layout
+		main_group.setLayout(main_layout)
 
 
 if __name__ == "__main__":
